33cf9345ecdee761a4e19a7a4bdb7cca01cd34e5
VixReee <53543117+VixReee@users.noreply.github.com>
2019-08-08 22:09:30 -0400

    Update bot-run.js

--- bot-run.js
+++ bot-run.js
@@ -1,18 +1,19 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on("ready", () => {
   // List servers the bot is connected to
   console.log("Connected as " + client.user.tag);
-//   client.guilds.forEach(guild => {
-//     console.log(" - " + guild.name);
+  client.user.setActivity("Anime", { type: "WATCHING" });
+  //   client.guilds.forEach(guild => {
+  //     console.log(" - " + guild.name);
 
-//     // List all channels
-//     guild.channels.forEach(channel => {
-//       console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
-//     });
-//   });
+  //     // List all channels
+  //     guild.channels.forEach(channel => {
+  //       console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+  //     });
+  //   });
 });
 
 client.login(config.token); /* Replace XXXXX with your bot token*/
