0198285ca65a5095f78a27411b62d011905ac52b
HurriXiii <corybeltran@yahoo.com>
2019-08-08 19:09:50 -0500

    added modules and stuff

---
+++ index.js
@@ -0,0 +1,6 @@
+const Discord = require('discord.js'); 
+const client = new Discord.Client();
+client.login(NjA5MTUyMDg3Njk1Mjk0NTI3.XUynBw.E_picnqGgJXx4kw9xOgxC6T-lAE);
+client.on('ready', () => {   
+    console.log('The bot is ready'); 
+});
