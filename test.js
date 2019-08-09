9f18c19cb6d976e1a821e6068f7439ff5fbe4cca
HurriXiii <corybeltran@yahoo.com>
2019-08-08 19:51:50 -0500

    redid base node install and deleted index

--- test.js
+++ test.js
@@ -1,19 +1,17 @@
-console.log("test2");
-
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on("ready", () => {
   // List servers the bot is connected to
-  console.log("Servers:");
-  client.guilds.forEach(guild => {
-    console.log(" - " + guild.name);
+  console.log("Connected as " + client.user.tag);
+//   client.guilds.forEach(guild => {
+//     console.log(" - " + guild.name);
 
-    // List all channels
-    guild.channels.forEach(channel => {
-      console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
-    });
-  });
+//     // List all channels
+//     guild.channels.forEach(channel => {
+//       console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`);
+//     });
+//   });
 });
 
-client.login("XXXXXXXXXXX"); // Replace XXXXX with your bot token
+client.login("NjA5MTUyMDg3Njk1Mjk0NTI3.XUzDAg.UCtOz5olgtoS9qvXE6kuJjQP_4M"); /* Replace XXXXX with your bot token*/
