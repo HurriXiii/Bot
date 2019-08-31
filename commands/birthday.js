d9727701cc5d8024981a627ce654ef31194601d6
HurriXiii <corybeltran@yahoo.com>
2019-08-30 19:36:35 -0500

    added birthday command and changed ready status

--- commands/birthday.js
+++ commands/birthday.js
@@ -4,6 +4,6 @@ module.exports = message =>
 {
     const embedBirthday = new RichEmbed()
         .setColor("FF8CE8")
-        .setTitle("I hope you have the bestest of birthdays commander!" + + "0w0 :tada: :birthday: :tada:");
+        .setTitle("I hope you have the bestest of birthdays commander!" + "0w0 :tada: :birthday: :tada:");
     message.channel.send(embedBirthday);
 };
--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ module.exports = bot =>
     console.log("deathmachine.exe booting up, codename: " + bot.user.tag);
     bot.user.setPresence({
         game: {
-            name: 'Animu',
+            name: 'Her Enemies Burn',
             type: "WATCHING",
             /*types include: watching, streaming, playing, listening*/
             //url: "https://www.twitch.tv/" // For Streaming Only
